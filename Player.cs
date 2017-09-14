8fdfde3b7fd8774db3026813bc0af1e68912c547
GabrielGameDev <gabrielbarbosa@msn.com>
2017-09-14 14:21:50 -0300

    Add files via upload

---
+++ Player.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+[System.Serializable]
+public class Boundary
+{
+	public float xMin, xMax, yMin, yMax;
+}
+
+public class Player : MonoBehaviour {
+
+	public float speed;
+	public Boundary boundary;
+
+	private Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+
+		rb = GetComponent<Rigidbody2D>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (CrossPlatformInputManager.GetButtonDown("Jump"))
+		{
+			GetComponent<SpriteRenderer>().enabled = !GetComponent<SpriteRenderer>().enabled;
+		}
+
+	}
+
+	private void FixedUpdate()
+	{
+		Vector2 movement = new Vector2(CrossPlatformInputManager.GetAxis("Horizontal"), CrossPlatformInputManager.GetAxis("Vertical"));
+		rb.velocity = movement * speed;
+
+		rb.position = new Vector2(Mathf.Clamp(rb.position.x, boundary.xMin, boundary.xMax), Mathf.Clamp(rb.position.y, boundary.yMin, boundary.yMax));
+	}
+}
