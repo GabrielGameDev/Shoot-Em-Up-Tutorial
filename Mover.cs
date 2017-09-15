9cb8d2fd2bef7473d75e09352e1fabef8bb59f06
GabrielGameDev <19438299+GabrielGameDev@users.noreply.github.com>
2017-09-15 14:11:53 -0300

    Add files via upload

---
+++ Mover.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour {
+
+	public float speed;
+
+	private Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+
+		rb = GetComponent<Rigidbody2D>();
+		rb.velocity = Vector2.up * speed;
+
+	}
+	
+	
+}
