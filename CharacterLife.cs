4cc8c5eecd8032070a7522672568e1f1ed0cbc38
GabrielGameDev <19438299+GabrielGameDev@users.noreply.github.com>
2017-09-25 17:37:02 -0300

    Add files via upload

---
+++ CharacterLife.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterLife : MonoBehaviour {
+
+	public int health;
+	public GameObject explosion;
+	public Color damageColor;
+
+	private bool isDead = false;
+	private SpriteRenderer sprite;
+
+	// Use this for initialization
+	void Start () {
+
+		sprite = GetComponent<SpriteRenderer>();
+
+	}
+	
+	public void TakeDamage(int damage)
+	{
+		if (!isDead)
+		{
+			health -= damage;
+			if(health <= 0)
+			{
+				Instantiate(explosion, transform.position, transform.rotation);
+				if(this.GetComponent<Player>() != null)
+				{
+
+				}
+				else
+				{
+					isDead = true;
+					Destroy(gameObject);
+				}
+			}
+			else
+			{
+				StartCoroutine(TakingDamage());
+			}
+		}
+	}
+
+	IEnumerator TakingDamage()
+	{
+		sprite.color = damageColor;
+		yield return new WaitForSeconds(0.1f);
+		sprite.color = Color.white;
+	}
+}
---
+++ DoDamage.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoDamage : MonoBehaviour {
+
+	public int damage = 1;
+
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		CharacterLife character = other.GetComponent<CharacterLife>();
+		if(character != null)
+		{
+			character.TakeDamage(damage);
+			Destroy(gameObject);
+		}
+	}
+}
