b2eb45c83de5b373226fc71862a845dbf2d1ca2f
GabrielGameDev <19438299+GabrielGameDev@users.noreply.github.com>
2017-10-11 16:53:19 -0300

    Add files via upload

---
+++ ItemDrop.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDrop : MonoBehaviour {
+
+	public ItemEffect effect;
+
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		Player player = other.GetComponent<Player>();
+		if(player != null)
+		{
+			player.SetItemEffect(effect);
+			Destroy(gameObject);
+		}
+	}
+}
